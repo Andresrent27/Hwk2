1fd7dfa55f83c1b4e75ea1471814cbeac09ec46b
Andres Renteria <andresrenteria@my.unt.edu>
2024-09-30 11:38:43 -0500

    initallized the file

---
+++ hwk2.cpp
@@ -0,0 +1,17 @@
+//Hwk2.cpp created by, Andres Renteria
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
